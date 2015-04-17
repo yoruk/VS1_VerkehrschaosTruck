109a7fdcdd931a3c5d9bb9eea9e5392f2c66255c
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2015-04-17 14:12:01 +0200

    wip #1

--- src/verkehrschaostruck/TruckImpl.java
+++ src/verkehrschaostruck/TruckImpl.java
@@ -4,35 +4,57 @@ import verkehrschaos.TruckCompany;
 import verkehrschaos.TruckPOA;
 
 public class TruckImpl extends TruckPOA {
-
+	private String name;
+	private TruckCompany company;
+	private double x;
+	private double y;
+	
+	public TruckImpl(String name) {
+		super();
+		this.name = name;
+	}
+	
 	@Override
+	/* Gibt den Namen des LKWs. */
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
+	/* Gibt die zugeordnete Spedition */
 	public TruckCompany getCompany() {
-		// TODO Auto-generated method stub
-		return null;
+		return company;
 	}
 
 	@Override
+	/* Neue Zuordnung einer Spedition.
+     * Wird von der zustaendigen Spedition aufgerufen. 
+     * Zu Debug-Zwecken soll der Name der neuen Spedition auf der Konsole ausgegeben werden. 
+     */
 	public void setCompany(TruckCompany company) {
-		// TODO Auto-generated method stub
-		
+		this.company = company;
 	}
 
 	@Override
+	/* Informiert den LKW waehrend einer Fahrt ueber die aktuelle Position.
+     * Position kann auf der Konsole ausgegeben werden.
+     * Wird von Streets aufgerufen.
+     */
 	public void setCoordinate(double x, double y) {
-		// TODO Auto-generated method stub
-		
+		this.x =  x;
+		this.y = y;
 	}
 
 	@Override
+	/*
+     * Stilllegung des LKWs (LKW Anwendung wird beendet).
+     * Wird z. B. von zugeordneter Spedition aufgerufen, wenn diese still gelegt wird
+     * oder von der Steueranwendung (Client).
+     * Beenden der Anwendung durch Aufruf von orb.shutdown(true).
+     * Nach orb.shutdown kleine Pause einlegen (0.5 sec) um Exception zu vermeiden.
+     */
 	public void putOutOfService() {
-		// TODO Auto-generated method stub
-		
+		System.exit(0);
 	}
 
 }
