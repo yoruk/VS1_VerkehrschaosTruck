a583a34e07b6cafb310146f0e8c3009e9678573a
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2015-04-17 12:42:16 +0200

    skelett fuer truckimpl erzeugt

---
+++ src/verkehrschaostruck/TruckImpl.java
@@ -0,0 +1,38 @@
+package verkehrschaostruck;
+
+import verkehrschaos.TruckCompany;
+import verkehrschaos.TruckPOA;
+
+public class TruckImpl extends TruckPOA {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TruckCompany getCompany() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCompany(TruckCompany company) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCoordinate(double x, double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putOutOfService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
